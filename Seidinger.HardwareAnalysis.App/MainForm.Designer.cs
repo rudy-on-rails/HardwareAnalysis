df85fd7073160a828657a26b4d738aaf92d0aa48
Rudy Seidinger <rudyseidinger@gmail.com>
2015-03-11 08:30:33 +0000

    Create the App Project for windows forms

---
+++ Seidinger.HardwareAnalysis.App/MainForm.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Seidinger.HardwareAnalysis.App
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.UpdateHardwareInformationTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AvailableMemoryLabel = new System.Windows.Forms.Label();
+            this.CpuUsageLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.HardDrivePercentageUsedLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 51.49425F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 48.50575F));
+            this.tableLayoutPanel1.Controls.Add(this.HardDrivePercentageUsedLabel, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.AvailableMemoryLabel, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.CpuUsageLabel, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.CloseButton, 1, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 14);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(474, 266);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CloseButton.Location = new System.Drawing.Point(370, 232);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(101, 31);
+            this.CloseButton.TabIndex = 0;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // UpdateHardwareInformationTimer
+            // 
+            this.UpdateHardwareInformationTimer.Interval = 1000;
+            this.UpdateHardwareInformationTimer.Tick += new System.EventHandler(this.UpdateHardwareInformationTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 10);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(10);
+            this.label1.Size = new System.Drawing.Size(188, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Available Memory";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(105, 76);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(10);
+            this.label2.Size = new System.Drawing.Size(136, 45);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CPU Usage";
+            // 
+            // AvailableMemoryLabel
+            // 
+            this.AvailableMemoryLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.AvailableMemoryLabel.AutoSize = true;
+            this.AvailableMemoryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AvailableMemoryLabel.Location = new System.Drawing.Point(247, 10);
+            this.AvailableMemoryLabel.Name = "AvailableMemoryLabel";
+            this.AvailableMemoryLabel.Padding = new System.Windows.Forms.Padding(10);
+            this.AvailableMemoryLabel.Size = new System.Drawing.Size(20, 45);
+            this.AvailableMemoryLabel.TabIndex = 3;
+            // 
+            // CpuUsageLabel
+            // 
+            this.CpuUsageLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.CpuUsageLabel.AutoSize = true;
+            this.CpuUsageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CpuUsageLabel.Location = new System.Drawing.Point(247, 76);
+            this.CpuUsageLabel.Name = "CpuUsageLabel";
+            this.CpuUsageLabel.Padding = new System.Windows.Forms.Padding(10);
+            this.CpuUsageLabel.Size = new System.Drawing.Size(20, 45);
+            this.CpuUsageLabel.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(117, 142);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(10);
+            this.label3.Size = new System.Drawing.Size(124, 45);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hard Drive";
+            // 
+            // HardDrivePercentageUsedLabel
+            // 
+            this.HardDrivePercentageUsedLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.HardDrivePercentageUsedLabel.AutoSize = true;
+            this.HardDrivePercentageUsedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HardDrivePercentageUsedLabel.Location = new System.Drawing.Point(247, 142);
+            this.HardDrivePercentageUsedLabel.Name = "HardDrivePercentageUsedLabel";
+            this.HardDrivePercentageUsedLabel.Padding = new System.Windows.Forms.Padding(10);
+            this.HardDrivePercentageUsedLabel.Size = new System.Drawing.Size(20, 45);
+            this.HardDrivePercentageUsedLabel.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 292);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Hardware Analysis Tool";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Timer UpdateHardwareInformationTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label AvailableMemoryLabel;
+        private System.Windows.Forms.Label CpuUsageLabel;
+        private System.Windows.Forms.Label HardDrivePercentageUsedLabel;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Seidinger.HardwareAnalysis.App/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using Seidinger.HardwareAnalysis.Core.Hardware;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seidinger.HardwareAnalysis.App
+{
+    public partial class MainForm : Form, HardwareResourceChangedObserver
+    {
+        private Computer computer;
+        public MainForm()
+        {
+            InitializeComponent();
+            this.computer = new Computer();
+            this.computer.AddHardwareResourceChangedObserver(this);
+            UpdateHardwareInformationTimer.Start();
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void UpdateHardwareInformationTimer_Tick(object sender, EventArgs e)
+        {
+            this.computer.UpdateHardwareInformation();
+        }
+
+        public void HardwareInformationChanged()
+        {
+            UpdateHardwareDisplayInformation();
+        }
+
+        private void UpdateHardwareDisplayInformation()
+        {
+            var cpuUsage = this.computer.CpuInformation.CpuUsage;
+            var availableMemory = this.computer.MemoryInformation.AvailableMemory;
+            var hardDiskUsage = this.computer.HardDriveInformation.HardDriveUsage;
+            this.CpuUsageLabel.Text = String.Format("{0} %", cpuUsage);
+            this.AvailableMemoryLabel.Text = String.Format("{0} MB", availableMemory);
+            this.HardDrivePercentageUsedLabel.Text = String.Format("{0} %", hardDiskUsage);
+        }
+    }
+}
